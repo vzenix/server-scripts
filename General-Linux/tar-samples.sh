7ce144f95e45ed0ab214bb28c300dc1cb5631f5f
Francisco M. E <paco@vzenix.es>
2018-02-22 08:46:57 +0100

    Update tar-samples.sh

--- General-Linux/tar-samples.sh
+++ General-Linux/tar-samples.sh
@@ -1,5 +1,5 @@
 
 # Pack Code
-tar -cvzf mywebsite.<Y>.<M>.<D>.es.tar.gz ./mywebsite.es
-sudo mv ./mywebsite.<Y>.<M>.<D>.es.tar.gz /home/user/backup_code/mywebsite.<Y>.<M>.<D>.tar.gz
+tar -cvzf mywebsite.Y.M.D.es.tar.gz ./mywebsite.es
+sudo mv ./mywebsite.Y.M.D.es.tar.gz /home/user/backup_code/mywebsite.Y.M.D.tar.gz
 
