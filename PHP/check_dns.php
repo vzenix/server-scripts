bdf64b041518604347fb79da4be95c1ba183de4b
Francisco M. E <paco@vzenix.es>
2021-08-24 08:40:56 +0200

    Update check_dns.php

--- PHP/check_dns.php
+++ PHP/check_dns.php
@@ -4,5 +4,5 @@ echo PHP_EOL;
 var_export(dns_get_record("vzenix.es"));
 echo PHP_EOL;
 $dns=array("8.8.8.8","8.8.4.4");
-var_export (dns_get_record ("mail.veridata.es", DNS_ALL, $dns));
+var_export (dns_get_record ("mail.vzenix.es", DNS_ALL, $dns));
 echo PHP_EOL; 
